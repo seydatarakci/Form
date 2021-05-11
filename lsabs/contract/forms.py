a711e3b2a9e3b4c17443d165571b5f381f5450e6
seydatarakci <78587060+seydatarakci@users.noreply.github.com>
2021-05-11 13:32:26 +0300

    Update forms.py

--- lsabs/contract/forms.py
+++ lsabs/contract/forms.py
@@ -7,18 +7,24 @@ from .models import *
 class ContractForm(forms.ModelForm):
 
     
-    firm_name=forms.CharField(label= 'Firm_Name ',max_length=80)
-  
-    axcode=forms.CharField(label= 'AxCode',max_length=3)
-
+    Firm_Name=forms.ModelChoiceField(queryset=Firm.objects.all(),label="Firm Name")
+    Limak_Firm=forms.ModelChoiceField(queryset=Firm.objects.all(),label="Limak Firm Name")
+    Firm_axcode=forms.ModelChoiceField(queryset=Subsidiary.objects.all(),label="Firm Axcode")
+    Limak_axcode=forms.ModelChoiceField(queryset=Subsidiary.objects.all(),label="Limak Firm Axcode")
     created_time = forms.DateTimeField(label='Tarih',widget=forms.SelectDateWidget)
-
-
-    
+    document=forms.FileField(label='Dosya')
+  
 
     class Meta:
         model = Contract
-        fields=('__all__')
+        fields=("Firm_Name","Limak_Firm","Firm_axcode","Limak_axcode","document")
+        
+
+    
+            
+         
+    
+
         
 
     
